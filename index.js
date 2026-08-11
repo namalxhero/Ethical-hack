552093b8a4bd2a89e1fd6f7235acf94e90389ddf
namalxhero <nipunanamal66@gmail.com>
2026-08-11 20:09:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -549,7 +549,7 @@ app.post('/chat', async (req, res) => {
             ]
         };
 
-        const url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" + apiKey;
+        const url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-3.5-flash-lite:generateContent?key=" + apiKey;
 
         const apiRes = await fetchFn(url, {
             method: 'POST',
